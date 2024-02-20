056346cb662a22b30f8ffe7c729a6c45159ed165
sohail <as19adj@herts.ac.uk>
2024-02-20 00:52:46 +0000

    adds repsonsive design for mobile devices

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,9 +1,18 @@
+import { motion as m, useScroll } from "framer-motion"
 import '../App.css'
 
+
 export default function MainSection(){
+    // for animation
+    const variants ={
+        visible:{opacity: 1},
+        hidden:{opacity: 0},
+    }
     return(
-        <section className='grid main-section-text-container'>
-            <p className='main-section-text'>Darya River is a Private company that offers all K-9 activities accordance with international standard procedures. All employees of this company are professionals in the K-9 department and have many years of work experience. The company is proud to train NDD and PDD and patrol dog and make them ready to serve of government, private institutions and Diplomatic quarters (embassies) etc.to provide it in the form of contract and rent. The company also has standardized and nationally and international accepted systems in the field of education and train of dog handlers including of men and women with the most experienced domestic teachers</p>
-        </section>
+        <m.section initial="hidden"
+        whileInView="visible"
+         className='grid main-section-text-container'
+            
+        />
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,50 @@
+import { useRef } from 'react';
+import { motion as m, useScroll} from "framer-motion"
 import '../App.css';
 export default function Navbar(){
+    const navRef = useRef();
+    const navBtn = useRef();
     const contact = "Get in touch";
+
+    // for animation
+    const variants ={
+        visible:{opacity: 1},
+        hidden:{opacity: 0},
+    }
+    const { scrollYProgress } = useScroll();
+
+   
+
     function openNav() {
-        document.getElementById("myNav").style.width = "100%";
-      }
-      
-      function closeNav() {
-        document.getElementById("myNav").style.width = "0%";
+        console.log(navBtn.current);
+        navBtn.current.classList.toggle("nav-btn-active")
+        navRef.current.classList.toggle('active');
+        document.body.classList.toggle('scroll-disabled');
+        
       }
     return(
-        <section className='display-animate'>
-            <div className="rectangle"></div>
+        <section  className='display-animate'>
+            <m.div
+        className="progress-bar"
+        style={{ scaleX: scrollYProgress }}
+      />
+            <m.div animate={{
+            scale: [1, 2, 2, 1, 1],
+            rotate: [0, 0, 270, 270, 0],
+            borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+            }} className="rectangle" />
             <header className="header">
                 <nav className="flex headerNav">
-                    <div className='width-60'>
+                    <div className='nav-width-60'>
                     <a id="header-nav-a-svg" href='#'><object className='headerSVG' data="./images/logo svg.svg" alt="logo"></object></a>
                     </div>
-                    <div className='width-40'>
-                        <div class='nav-btn'>
-                            <label for="nav-check" className='nav-btn-label'>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            </label>
-                        </div>
-                    <ul className='flex headerList no-bullets'>
+                    <a class='nav-btn' id='navbar-toggle' onClick={openNav} ref={navBtn}>
+                        <span className='bar'></span>
+                        <span className='bar'></span>
+                        <span className='bar'></span>
+                    </a>
+                    <div className='nav-width-40' ref={navRef}>
+                    <ul className='flex nav-links no-bullets'>
                         <li><a className='no-underline bold' href="#">Home</a></li>
                         <li><a className='no-underline bold' href="#">About us</a></li>
                         <li><a className='no-underline bold' href="#">Contact</a></li>
